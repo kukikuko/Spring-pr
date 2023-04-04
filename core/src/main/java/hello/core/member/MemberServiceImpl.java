ef010b30b2ff0959b822101cd8cc85d9245a6545
kukikuko <gurwn805@naver.com>
2023-04-04 20:38:07 +0900

    멤버 회원가입 기능 할인 기능 작업

---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+	private final MemberRepository memberRepository = new MemoryMemberRepository();
+	
+	@Override
+	public void join(Member member) {
+		// TODO Auto-generated method stub
+		memberRepository.save(member);
+	}
+
+	@Override
+	public Member findMember(Long memberId) {
+		// TODO Auto-generated method stub
+		return memberRepository.findById(memberId);
+	}
+
+}
